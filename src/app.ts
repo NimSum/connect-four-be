2d24bd7a11477357006f4a7454458728d410959f
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-05 16:03:16 -0500

    Setup basic express sever and socket io

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
 const mongoose = require('mongoose');
 const { mongoURI } = require('../config/keys');
-const client = require('socket.io').listen(3000).sockets;
+const socket = require('socket.io');
+const express = require('express');
+
+const app = express();
+const server = app.listen(3000, () => {
+  console.log('Listening to requests on port 3000')
+});
+const io = socket(server);
+
+io.on('connection', (socket: object) => {
+  console.log(socket)
+});
 
 mongoose.connect(
   mongoURI,
