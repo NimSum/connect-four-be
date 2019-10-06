f3f8abd1903a13d6b2f67c6dd87c1752f4b246c1
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-06 12:54:14 -0500

    Create handler and gameroom files

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,6 @@ const express = require('express');
 const app = express();
 const server = app.listen(3000, (err: any) => {
   if (err) throw err;
-
   console.log('Listening to requests on port 3000')
 });
 
@@ -47,34 +46,34 @@ io.on('error', (err: string) => {
   console.log(`Recieved socket error: /n ${err}`)
 });
 
-function handleRegister() {
-
+function handleRegister(onGridRecieved) {
+  io.on('gridUpdate', onGridRecieved);
 };
 
 function unregisterHandler() {
-
+  io.off('gridUpdate');
 };
 
 function register(name, cb) {
-
+  io.emit('register', name, cb);
 };
 
 function handleJoin(gameRoomName, cb) {
-
+  io.emit('join', gameRoomName, cb);
 };
 
 function handleLeave(gameRoomName, cb) {
-
+  io.emit('leave', gameRoomName, cb);
 };
 
 function handleGridUpdate(gameRoomName, grid, cb) {
-
+  io.emit('gridUpdate', { gameRoomName, grid }, cb );
 };
 
 function getGameRooms(cb) {
-
+  io.emit('gameRooms', null, cb);
 };
 
 function getAvailablePlayers(cb) {
-
+  io.emit('availablePlayers', null, cb);
 };
