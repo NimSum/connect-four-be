877689105f9cfc2c6218717f62554378edea3b13
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-09 14:27:04 -0500

    Create mongoDB player schema

--- src/db/models.ts
+++ src/db/models.ts
@@ -3,7 +3,20 @@ export {};
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const playerSchema = new Schema();
+const playerSchema = new Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  player_name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  secret_one: { type: String, required: true },
+  secret_two: { type: String, required: true },
+  win_streak: { type: Number, default: 0 },
+  wins: { type: Number, default: 0 },
+  losses: { type: Number, default: 0 },
+  games_played: { type: Number, default: 0 },
+  friends: [Number],
+  achievements: [Number],
+}, { timestamps: { createdAt: 'created_at' } });
 
 module.exports = {
   Player: mongoose.model('Player', playerSchema)
