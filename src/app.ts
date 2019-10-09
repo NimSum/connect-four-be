6d576fdc65e9a03795f0c9028fbcfe78c22f6771
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-09 14:24:06 -0500

    Router now longer a function

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,9 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
-app.use('/api/v1', router());
+app.use('/api/v1', router);
 
 const server = app.listen(3000, (err: any) => {
   if (err) throw err;
   console.log('Listening to requests on port 3000')
 });
-
-module.exports = app;
