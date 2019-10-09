563d4bfd34e489ca1cc2e382f9dbb5bd46a417f7
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-08 23:08:58 -0500

    Creates signup route, param checking and response

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -2,6 +2,16 @@ module.exports = function() {
   const express = require('express');
   const router = express.Router();
   const utils = require('../utils')();
+  
+  router.post('/signup', (req, res) => {
+    const verifiedParams = utils.checkSignupParams(req.body);
+    if (verifiedParams === true) {
+      // create account
+      res.status(201).json('Account Created');
+    } else {
+      res.status(400).json(verifiedParams);
+    };
+  }); 
 
   return router;
 };
