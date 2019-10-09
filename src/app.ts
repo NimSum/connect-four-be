f0e06630987f49a47e2c46c9b0045f707e48e56d
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-08 22:37:40 -0500

    Install and use body-parser

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 const express = require('express');
+const router = require('./server/routes');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use('/api/v1', router());
 
 const server = app.listen(3000, (err: any) => {
   if (err) throw err;
   console.log('Listening to requests on port 3000')
 });
 
-app.use(express.static('public'));
+module.exports = app;
