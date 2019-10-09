e60cb64da0b995fb73a994703c9c5ddd75d0e3df
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-08 22:59:20 -0500

    Create signup parameter verification function

--- src/server/utils/index.ts
+++ src/server/utils/index.ts
@@ -1,3 +1,27 @@
 module.exports = function() {
-  
+  function checkSignupParams(params: Object): Boolean | object {
+    const requiredParams = [
+      'username',
+      'email',
+      'password',
+      'secret_one',
+      'secret_two'
+    ];
+    const missingParams = [];
+    
+    for (let param of requiredParams) {
+      if (!params[param]) missingParams.push(param);
+    };
+
+    return missingParams.length > 0
+      ? {
+          error: 'Missing required params',
+          missing: missingParams.join(', ')
+        }
+      : true;
+  };
+
+  return {
+    checkSignupParams
+  };
 };
