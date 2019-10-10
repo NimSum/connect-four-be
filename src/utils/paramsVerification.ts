1336da5de9fceee0ed2cd060f3de2118cc5591d0
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-10 12:37:01 -0500

    Re-factor param verification to use helper function

--- src/utils/paramsVerification.ts
+++ src/utils/paramsVerification.ts
@@ -1,17 +1,20 @@
 const requiredParams = require('./requiredParams');
 
-function checkSignupParams(params: Object): Boolean | object {
+function checkSignupParams(params: Object): Boolean | string | Boolean {
+  const missingParams = checkParams(params, requiredParams.signUp);
+
+  return missingParams;
+};
+
+function checkParams(params: Object, required: Array<string>): string | Boolean {
   const missingParams: Array<string> = [];
-  
-  for (let param of requiredParams.signUp) {
+
+  for (let param of required) {
     if (!params[param]) missingParams.push(param);
   };
 
   return missingParams.length > 0
-    ? {
-        error: 'Missing required params',
-        missing: missingParams.join(', ')
-      }
+    ? missingParams.join(', ')
     : true;
 };
 
