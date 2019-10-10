779a7fb051aa26b5fd8a6371685a61cbceb68bbf
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-10 14:58:42 -0500

    Verify user and password in db and check password hashes

--- src/server/middleware/loginAuthentication.ts
+++ src/server/middleware/loginAuthentication.ts
@@ -1,9 +1,25 @@
 export{};
 
 const db = require('../../db');
+const { compareEncryptedText } = require('../../utils/passwordEncryptions')
 
 async function loginAuthentication(req, res, next) {
+  try {
+    const playerFound = await db.getPlayer(req.body);
+    const { password } = playerFound;
+    const match = await compareEncryptedText(req.body.password, password);
 
+    if (playerFound === null || !match) {
+      res.status(404).json({ 
+        error: 'Invalid login credentials' 
+      });
+    } else {
+      req.playerFound = playerFound;
+      next();
+    };
+  } catch(error) { 
+    res.status(500).json(error);
+  };
 };
 
 module.exports = loginAuthentication;
