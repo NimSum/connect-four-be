dc336c008bfb369c41db0cc54bc3eb7b751571f0
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-10 14:23:04 -0500

    Convert to async await and try catch blocks

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -5,16 +5,22 @@
   const params = require('../../utils/paramsVerification');
   const db = require('../../db');
   
-  router.post('/signup', params.checkSignupParams, (req: any, res:any) => {
-      db.createNewPlayer(req.body)
-        .then((result: object) => res.status(201).json(result))
-        .catch((error: object) => res.status(500).json(error));
+  router.post('/signup', params.checkSignupParams, async (req: any, res:any) => {
+    try {
+      const newPlayer = await db.createNewPlayer(req.body);
+      res.status(201).json(newPlayer);
+    } catch(error) {
+      res.status(500).json(error)
+    }
   });
 
-  router.post('/login', params.checkLoginParams, (req:any, res:any) => {
-      db.getPlayer(req.body)
-        .then((result: object) => res.status(200).json(result))
-        .catch((error: object) => res.status(500).json(error));
+  router.post('/login', params.checkLoginParams, async (req:any, res:any) => {
+      try {
+        const playerFound = await db.getPlayer(req.body);
+        res.status(200).json(playerFound)
+      } catch(error){
+        res.status(500).json(error);
+      }
   });
 
   module.exports = router;
