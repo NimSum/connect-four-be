ac0d37bc9c4c22a23dd23463e37b08d37878c114
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-11 13:15:45 -0500

    Test account creation in db

--- test/app.test.js
+++ test/app.test.js
@@ -6,7 +6,14 @@ const mongoose = require('mongoose');
 describe('Player Authentication', () => {
 
   it('creates a player', (done) => {
- 
+    let newPlayer = new Player({ ...mockData.newPlayer });
+    newPlayer._id = new mongoose.Types.ObjectId();
+    
+    newPlayer.save() 
+      .then(() => {
+          assert(!newPlayer.isNew); 
+          done();
+      });
   });
 
 });
