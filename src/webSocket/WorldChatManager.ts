b10d613fbc70cc99e9d1cd8f3cf48871d89f929b
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-31 17:03:52 -0500

    Create message broadcast function, closes #18

--- src/webSocket/WorldChatManager.ts
+++ src/webSocket/WorldChatManager.ts
@@ -3,6 +3,17 @@ const chatContainer: Array<ChatHistoryItem> = [];
 
 module.exports = function(client: any, io: any) {
   function broadcastMessage(message: string) {
+    if (message.length > 1) {
+      const { player_name } = players.get(client.id) || {};
+    const setMessage = { message, player_name, timestamp: Date.now() }
+    chatContainer.push(setMessage);
+
+    const update: ChatHistoryItem = { type: 'message', ...setMessage };
+    broadcastToWorld(update);
+  }
+  }
+  
+  
 
   return {
     broadcastToWorld,
