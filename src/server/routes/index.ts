5da243d228ad7e27d0ef1640decef067f4a5d300
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-31 22:52:15 -0500

    Create anonymous login route and response, closes #38

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -22,7 +22,18 @@
     const { player_name, email } = req.playerFound;
     const token = await jwt.generateToken({ player_name, email });
 
-    res.status(200).json({token, player: req.playerFound});
+  router.post('/anonymous', async (req:any, res:any) => {
+    const { player_name } = req.body;
+    try {
+      if (!!player_name && player_name.length > 2) {
+        const token = await jwt.generateToken({ player_name, player_type: 'anonymous' });
+        res.status(200).json({token});
+      } else {
+        res.status(400).json({error: 'Name must be 3-15 characters'})
+      }
+    } catch(error) {
+      res.status(500).json(error);
+    }
   });
 
   module.exports = router;
