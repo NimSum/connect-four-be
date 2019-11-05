2c9015b412a90b4affbcdc11e6a4a7a6247c76d4
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-04 18:27:31 -0600

    Update production secret key key

--- dist/config/keys_prod.js
+++ dist/config/keys_prod.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
-    secretOrKey: process.env.SECRET_JWT_KEY
+    secretKey: process.env.SECRET_JWT_KEY
 };
 //# sourceMappingURL=keys_prod.js.map
