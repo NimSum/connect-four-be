d600fcc035911ff63790f9f6532c5325d3ff2ebf
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-15 15:45:42 -0600

    Prettyfies code

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,91 +1,87 @@
 export {};
 
-const mongoose = require('mongoose');
-const { mongoURI } = require('../../config/keys');
-const { Player } = require('./models');
-const { encryptText } = require('../utils/passwordEncryptions');
+const mongoose = require("mongoose");
+const { mongoURI } = require("../../config/keys");
+const { Player } = require("./models");
+const { encryptText } = require("../utils/passwordEncryptions");
 
 const { ObjectId } = mongoose.Types;
 
-mongoose.connect(
-  mongoURI,
-  { 
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-).catch((error: any) => {
-    throw new Error(error)
-  }
-);
+mongoose
+	.connect(mongoURI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false
+	})
+	.catch((error: any) => {
+		throw new Error(error);
+	});
 
 async function createNewPlayer(player: Player) {
-  const { player_name, email, password, secret_one, secret_two } = player;
-  
-  const newPlayer = new Player({
-    _id: new mongoose.Types.ObjectId(),
-    player_name,
-    email,
-    password: await encryptText(password),
-    secret_one,
-    secret_two
-  });
+	const { player_name, email, password, secret_one, secret_two } = player;
 
-  return await newPlayer.save();
-};
+	const newPlayer = new Player({
+		_id: new mongoose.Types.ObjectId(),
+		player_name,
+		email,
+		password: await encryptText(password),
+		secret_one,
+		secret_two
+	});
 
-async function getPlayer(isEmail: boolean, player: string) {
-  const type: string = isEmail ? 'email' : 'player_name';
-  return await Player.findOne({ [type]: player });
-};
-
-async function getPlayerById(id: string) {
-  return await Player.find(ObjectId(id));
-};
-
-async function winnerStatUpdate(id: string, opponent: Opponent) {
-  return await Player.findOneAndUpdate(
-    {_id: id}, 
-    {
-      $inc : {'games_played': 1, 'wins': 1, 'win_streak': 1},
-      $push: {'game_history': opponent}
-    }
-  )
-};
-
-async function loserStatUpdate(id: string, opponent: Opponent) {
-  return await Player.findOneAndUpdate(
-    {_id: id}, 
-    {
-      $inc : {'games_played': 1, 'losses': 1},
-      $push: {'game_history': opponent},
-      'win_streak': 0,
-    }
-  );
-};
-
-
-
-module.exports = {
-  createNewPlayer,
-  getPlayer,
-  getPlayerById,
-  winnerStatUpdate,
-  loserStatUpdate
+	return await newPlayer.save();
 }
 
+async function getPlayer(isEmail: boolean, player: string) {
+	const type: string = isEmail ? "email" : "player_name";
+	return await Player.findOne({ [type]: player });
+}
+
+async function getPlayerById(id: string) {
+	return await Player.find(ObjectId(id));
+}
+
+async function winnerStatUpdate(id: string, opponent: Opponent) {
+	return await Player.findOneAndUpdate(
+		{ _id: id },
+		{
+			$inc: { games_played: 1, wins: 1, win_streak: 1 },
+			$push: { game_history: opponent }
+		}
+	);
+}
+
+async function loserStatUpdate(id: string, opponent: Opponent) {
+	return await Player.findOneAndUpdate(
+		{ _id: id },
+		{
+			$inc: { games_played: 1, losses: 1 },
+			$push: { game_history: opponent },
+			win_streak: 0
+		}
+	);
+}
+
+module.exports = {
+	createNewPlayer,
+	getPlayer,
+	getPlayerById,
+	winnerStatUpdate,
+	loserStatUpdate
+};
+
 interface Player {
-  player_name: string,
-  email: string, 
-  password: string, 
-  secret_one: string, 
-  secret_two: string
+	player_name: string;
+	email: string;
+	password: string;
+	secret_one: string;
+	secret_two: string;
 }
 
 interface Opponent {
-  vs_player: string,
-  vs_player_id: string,
-  is_winner: boolean,
-  created_at: string
+	vs_player: string;
+	vs_player_id: string;
+	is_winner: boolean;
+	created_at: string;
 }
--- src/db/models.ts
+++ src/db/models.ts
@@ -1,31 +1,34 @@
 export {};
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const playerSchema = new Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  player_name: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  secret_one: { type: String, required: true },
-  secret_two: { type: String, required: true },
-  win_streak: { type: Number, default: 0 },
-  wins: { type: Number, default: 0 },
-  losses: { type: Number, default: 0 },
-  games_played: { type: Number, default: 0 },
-  friends: [String],
-  achievements: [Number],
-  game_history: [
-    {
-      vs_player: String,
-      vs_player_id: String,
-      is_winner: Boolean,
-      created_at: Date
-    }
-  ]
-}, { timestamps: { createdAt: 'created_at' } });
+const playerSchema = new Schema(
+	{
+		_id: mongoose.Schema.Types.ObjectId,
+		player_name: { type: String, required: true, unique: true },
+		email: { type: String, required: true, unique: true },
+		password: { type: String, required: true },
+		secret_one: { type: String, required: true },
+		secret_two: { type: String, required: true },
+		win_streak: { type: Number, default: 0 },
+		wins: { type: Number, default: 0 },
+		losses: { type: Number, default: 0 },
+		games_played: { type: Number, default: 0 },
+		friends: [String],
+		achievements: [Number],
+		game_history: [
+			{
+				vs_player: String,
+				vs_player_id: String,
+				is_winner: Boolean,
+				created_at: Date
+			}
+		]
+	},
+	{ timestamps: { createdAt: "created_at" } }
+);
 
 module.exports = {
-  Player: mongoose.models.player || mongoose.model('player', playerSchema)
+	Player: mongoose.models.player || mongoose.model("player", playerSchema)
 };
