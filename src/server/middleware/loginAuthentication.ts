a16aaa5534b06a1486c14f462f2e611c46e8d165
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-15 15:46:07 -0600

    Prettyfies code

--- src/server/middleware/loginAuthentication.ts
+++ src/server/middleware/loginAuthentication.ts
@@ -1,38 +1,40 @@
-export{};
+export {};
 
-const db = require('../../db');
-const { compareEncryptedText } = require('../../utils/passwordEncryptions')
-const { verifyToken } = require('../../utils/jwtAuthentication');
+const db = require("../../db");
+const { compareEncryptedText } = require("../../utils/passwordEncryptions");
+const { verifyToken } = require("../../utils/jwtAuthentication");
 
 async function loginAuthentication(req: any, res: any, next: any) {
-  let playerFound: any;
+	let playerFound: any;
 
-  try {
-    if (!!req.headers.authorization) {
-      const verified =  await verifyToken(req.headers.authorization);
-      const id: string = verified._id;
-      const found = await db.getPlayerById(id);
-      playerFound = found[0];
-    } else {
-      const email: string = req.body.email;
-      playerFound = await db.getPlayer(true, email)
-    }
+	try {
+		if (!!req.headers.authorization) {
+			const verified = await verifyToken(req.headers.authorization);
+			const id: string = verified._id;
+			const found = await db.getPlayerById(id);
+			playerFound = found[0];
+		} else {
+			const email: string = req.body.email;
+			playerFound = await db.getPlayer(true, email);
+		}
 
-    if (!playerFound) {
-      res.status(404).json({ error: 'Invalid login credentials' });
-    } else {
-      const passwordMatch: boolean = await compareEncryptedText(req.body.password || '', playerFound.password || '');
-      if (passwordMatch || !!req.headers.authorization) {
-        req.playerFound = playerFound
-        next();
-      }
-      else {
-        res.status(404).json({ error: 'Invalid login credentials' })
-      };
-    };
-  } catch(error) { 
-    res.status(500).json(error);
-  };
-};
+		if (!playerFound) {
+			res.status(404).json({ error: "Invalid login credentials" });
+		} else {
+			const passwordMatch: boolean = await compareEncryptedText(
+				req.body.password || "",
+				playerFound.password || ""
+			);
+			if (passwordMatch || !!req.headers.authorization) {
+				req.playerFound = playerFound;
+				next();
+			} else {
+				res.status(404).json({ error: "Invalid login credentials" });
+			}
+		}
+	} catch (error) {
+		res.status(500).json(error);
+	}
+}
 
 module.exports = loginAuthentication;
--- src/server/middleware/paramsVerification.ts
+++ src/server/middleware/paramsVerification.ts
@@ -1,29 +1,29 @@
-const requiredParams = require('../../utils/requiredParams');
+const requiredParams = require("../../utils/requiredParams");
 
 function checkParams(req: any, res: any, next: any, required: Array<string>) {
-  const missingParams: Array<string> = [];
-  
-  const ifTokenLogin = req.path === '/login' && !!req.headers.authorization;
+	const missingParams: Array<string> = [];
 
-  for (let param of required) {
-    if (!req.body[param]) missingParams.push(param);
-  };
+	const ifTokenLogin = req.path === "/login" && !!req.headers.authorization;
 
-  if (missingParams.length > 0 && !ifTokenLogin) {
-    const errorMessage = {
-      params_required: required.join(', '),
-      error: `Missing: ${missingParams.join(', ')}`
-    }
+	for (let param of required) {
+		if (!req.body[param]) missingParams.push(param);
+	}
 
-    res.status(400).json(errorMessage);
-  } else {
-    next();
-  }
-};
+	if (missingParams.length > 0 && !ifTokenLogin) {
+		const errorMessage = {
+			params_required: required.join(", "),
+			error: `Missing: ${missingParams.join(", ")}`
+		};
+
+		res.status(400).json(errorMessage);
+	} else {
+		next();
+	}
+}
 
 module.exports = {
-  checkSignupParams: (req: any, res: any, next: any) => 
-    checkParams(req, res, next, requiredParams.signUp),
-  checkLoginParams: (req: any, res: any, next: any) => 
-    checkParams(req, res, next, requiredParams.login)
+	checkSignupParams: (req: any, res: any, next: any) =>
+		checkParams(req, res, next, requiredParams.signUp),
+	checkLoginParams: (req: any, res: any, next: any) =>
+		checkParams(req, res, next, requiredParams.login)
 };
